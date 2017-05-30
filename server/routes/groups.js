972ecab5b237dc9df43c61f8075a8a4e69ffdb83
Aly-Tamboura <alytamboura7370@gmail.com>
2017-05-30 14:30:51 -0700

    added tabscroll

---
+++ server/routes/groups.js
@@ -0,0 +1,13 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const GroupController = require('../controllers').Profiles;
+
+router.route('/')
+  .get((req, res) => {
+  })
+  .post((req, res) => {
+    GroupController.checkDestinations(req, res)
+  });
+
+module.exports = router;
