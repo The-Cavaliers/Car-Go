5a0b18c0d44fe4a4b2d987fb7e28f68dea08aa49
Phongtlam <Phongtlam@gmail.com>
2017-05-31 11:09:30 -0700

    rebase

--- client/components/loginScreen.js
+++ client/components/loginScreen.js
@@ -19,10 +19,95 @@ const lock = new Auth0Lock({
   domain: CONFIG.auth0.domain,
 });
 
+
+// class Login extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       username: '',
+//       password: '',
+//     };
+//
+//     this._login = this._login.bind(this);
+//   }
+//   componentWillMount() {
+//     this.FbLogin();
+//   }
+//
+//   _login() {
+//     fetch('http://127.0.0.1:3000/mobile', {
+//       method: 'POST',
+//       headers: {
+//         Accept: 'application/json',
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         username: this.state.username,
+//         password: this.state.password,
+//       }),
+//     })
+//         .then(response => (response.json()))
+//         .then((res) => {
+//           if (res.success === true) {
+//             const username = res.message;
+//             AsyncStorage.setItem('username', username);
+//             this.props.navigation.navigate('Profile');
+//           } else {
+//             alert('no user found');
+//           }
+//         })
+//         .catch((err) => {
+//           alert(err);
+//         });
+//   }
+//
+//   FbLogin() {
+//     lock.show({}, (err, profile, token) => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       // Authentication worked!
+//       console.log('Logged in with Auth0!');
+//     });
+//   }
+//
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Image
+//           style={{ width: 150, height: 150 }}
+//           source={require('../assets/carpool.png')}
+//         />
+//         <Text style={styles.welcome}> Carpool Application</Text>
+//         <View style={styles.inputContainer}>
+//           <TextInput
+//             underlineColorIos="transparent" style={styles.input}
+//             onChangeText={username => this.setState({ username })}
+//             value={this.state.username}
+//             placeholder="username"
+//           />
+//           <TextInput
+//             secureTextEntry={true}
+//             underlineColorIos="transparent"
+//             style={styles.input}
+//             onChangeText={password => this.setState({ password })}
+//             value={this.state.password}
+//             placeholder="password"
+//           />
+//           <TouchableOpacity onPress={this._login} style={styles.buttonContainer}>
+//             <Text style={styles.buttonText}> Login</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </View>
+//     );
+//   }
+// }
+
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: '' };
+    this.state = { name: '', picture: null }
   }
 
   componentWillMount() {
@@ -38,6 +123,7 @@ class Login extends Component {
       this.setState({ name: profile.name });
       console.log('profile:', profile);
       console.log('token:', token);
+
     });
   }
 
