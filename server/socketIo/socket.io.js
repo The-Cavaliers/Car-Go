a6aab57de45afb9410fd5c9f9c6f407ee2323bcd
Phongtlam <Phongtlam@gmail.com>
2017-06-05 16:41:02 -0700

    preparing for rebase

--- server/socketIo/socket.io.js
+++ server/socketIo/socket.io.js
@@ -1,20 +1,36 @@
 const socketIo = require('socket.io');
 
+const CONFIG = require('../../config/development.json');
+const knex = require('knex')(CONFIG.knex_config);
+
 module.exports = (server) => {
   const websocket = socketIo(server);
 
-  websocket.on('connection', (socket) => {
-    console.log('A client just joined on', socket.id);
-    // socket.on('CarGo', message => console.log(message));
+  websocket.on('connect', (socket) => {
+    console.log('A client just joined on (socket.id)', socket.id);
+    // client[socket.id] = socket;
+    // console.log('client is', client)
+
+    // socket.on('userJoined', (userId) => {
+    //   // onUserJoined(userId, socket);
+    //   console.log('user id', userId)
+    //   if (!userId) {
+    //     socket.emit('userJoined', socket.id);
+    //     // returnMessage(socket); wrong need a diff function
+    //   }
+    //   // returnMessage(socket);
+    // });
 
     socket.on('message', (message) => {
-      socket.broadcast.emit('message', message);
-      console.log(message);
+      console.log('message from server', message);
+      const messageData = {
+        _id: message._id,
+        text: message.text,
+        user: message.user,
+        createdAt: new Date(message.createdAt),
+        // roomId,
+      };
+      socket.broadcast.emit('message', messageData);
     });
-    // getuser
-     // get the messages from the client
-    // socket.on('add-user', (data) => {
-    //   console.log(data);
-    // });
   });
 };
