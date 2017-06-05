cb5a32db4a70d643f9975d129da94fea52ead67e
Phongtlam <Phongtlam@gmail.com>
2017-06-04 18:19:10 -0700

    new chatterbox

--- client/components/ChatterBox.js
+++ client/components/ChatterBox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AsyncStorage } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 import SocketIOClient from 'socket.io-client';
 import CONFIG from '../../config/development.json';
@@ -8,51 +9,70 @@ class ChatterBox extends React.Component {
     super(props);
     this.state = { messages: [] };
     this.onSend = this.onSend.bind(this);
-    // Socket IO connection
-    this.socket = SocketIOClient(CONFIG.URL);
-    this.socketMessages = [];
+    this.storeMessages = this.storeMessages.bind(this);
+    this.onReceivedMessage = this.onReceivedMessage.bind(this);
     // Keeps listening to the server side message emission;
-    // this.socket.on('message', this.onReceivedMessage);
+    this.socket = SocketIOClient(CONFIG.URL);
+    this.socket.on('message', this.onReceivedMessage);
     // initial user details
-    this.socket.emit('add-user', { username: 'userName', groupname: 'groupName' });
   }
+
   componentWillMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Welcome to chat!', // input global state name here
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://facebook.github.io/react/img/logo_og.png',
+    AsyncStorage.getItem('AsyncProfile', (err, result) => {
+      const AsyncProfile = JSON.parse(result);
+      console.log('AsyncProfile', AsyncProfile);
+      this.setState({
+        messages: [
+          {
+            _id: 1,
+            text: `Hi ${AsyncProfile.username}!`, // input global state name here
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: 'React Native',
+              avatar: 'https://facebook.github.io/react/img/logo_og.png',
+            },
           },
-        },
-      ],
+        ],
+      });
     });
   }
-  onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
 
-   }));
-    // emit the messages to server
-    this.socket.emit('message',
-      {
-        message: messages,
-        date: 'today',
-        userName: 'May',
-        groupName: 'ABC',
-      });
-    this.socket.on('message');
+  componentDidMount() {
+    // this.socket.emit('add-user', { username: 'userName', groupname: 'groupName' });
+    // this.socketMessages = [];
   }
+
+  onSend(messages = []) {
+    // this.setState(previousState => ({
+    //   messages: GiftedChat.append(previousState.messages, messages),
+    // }));
+    // // emit the messages to server
+    // this.socket.emit('message',
+    //   {
+    //     message: messages,
+    //     date: 'today',
+    //     userName: 'May',
+    //     groupName: 'ABC',
+    //   });
+    // this.socket.on('message');
+    this.socket.emit('message', messages[0]);
+    this.storeMessages(messages);
+  }
+
   onReceivedMessage(messages) {
     console.log(messages);
-    this.setState({
-      // socketMessages: this.state.socketMessages.concat([messages]),
-    });
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
   }
+
+  storeMessages(messages) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
   render() {
     return (
       <GiftedChat
