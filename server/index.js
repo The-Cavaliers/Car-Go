a66dc3d738f84b0a0678870e3f3bf45353990e93
Phongtlam <Phongtlam@gmail.com>
2017-06-04 18:17:46 -0700

    first commit

--- server/index.js
+++ server/index.js
@@ -1,25 +1,11 @@
 const app = require('./app');
 const server = require('http').Server(app);
-const websocket = require('socket.io')(server);
+const websocket = require('socket.io')
+
+websocket(server);
 
 const PORT = process.env.port || 3000;
 
 server.listen(PORT, () => {
   console.log(`GoCar listening on port ${PORT}!`);
-});
-
-websocket.on('connection', (socket) => {
-  console.log('A client just joined on', socket.id);
-  socket.on('CarGo', message => console.log(message));
-
-  // get the messages from the client
-  socket.on('message', (data) => {
-    // websocket.emit('message', { username: data.userName, message: data.message, groupName: data.groupName, date: data.date });
-    console.log(data);
-  });
-  //getuser
-   // get the messages from the client
-  socket.on('add-user', (data) => {
-    console.log(data);
-  });
 });
