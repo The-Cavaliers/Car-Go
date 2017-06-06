a45ab1f79f6a378ac6b1007af732ef72dd8f3478
Phongtlam <Phongtlam@gmail.com>
2017-06-06 11:59:08 -0700

    updated chat

--- server/socketIo/message.js
+++ server/socketIo/message.js
@@ -1,10 +1,11 @@
 module.exports = (socket) => {
-  console.log('A client just joined on (socket.id)', socket.id);
+  // console.log('A client just joined on (socket.id)', socket.id);
 
+  // main sending message module
   socket.on('message', (message) => {
     console.log('message from server', message);
     const messageData = {
-      _id: message._id,
+      _id: socket.id,
       text: message.text,
       user: message.user,
       createdAt: new Date(message.createdAt),
