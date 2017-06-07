ea7b3e189600fac7f15057e0324cfa7242571e19
Mahima Srikanta <mahima.srikanta@gmail.com>
2017-06-07 10:47:04 -0700

    GeoCoder Configuration

--- server/controllers/maps.js
+++ server/controllers/maps.js
@@ -1,17 +1,27 @@
-const CONFIG = require('../../config/development.json');
-const knex = require('knex')(CONFIG.knex_config);
+
+const models = require('../../db/models');
+const dbConfig = require('../dbConfig.js');
+const geocoder = require('geocoder');
+const knex = require('knex')(dbConfig);
+
 
 module.exports.getMapPins = (req, res) => {
-  knex('groups').where({
-    leaving_from: 'San Francisco',
-  }).select('*')
+  console.log('Response', req.query);
+
+// Reverse Geocoding
+  geocoder.reverseGeocode(37.783692, -122.408967, (err, data) => {
+  // do something with data
+    console.log(data.results[0].address_components[3].long_name);
+    knex('groups').where({
+      leaving_from: data.results[0].address_components[3].long_name,
+    }).select('*')
   .then((response) => {
-    //console.log(response);
+    // console.log(response);
     res.send(response);
   })
   .catch((error) => {
-    console.log(error);
+    // console.log(error);
     res.send(error);
   });
-
+  }, { key: 'AIzaSyCyMh2NG1WVqO3fwmrFA7km3Vgwu24YmYI' });
 };
