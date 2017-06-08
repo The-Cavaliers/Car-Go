48c2e54459d1f6276305f5a1444c48fd5293792c
Phongtlam <Phongtlam@gmail.com>
2017-06-08 15:43:51 -0700

    chatter room working

--- client/components/routing.js
+++ client/components/routing.js
@@ -1,14 +1,8 @@
 import { StackNavigator, addNavigationHelpers } from 'react-navigation';
 import Drawer from './DrawerNav';
-import { UserProfile } from './UserProfile';
-import Groups from './Groups';
 
 const AppNavigator = StackNavigator({
-  // Login: { screen: Login },
-  // Profile: { screen: Profile },
   Drawer: { screen: Drawer },
-  UserProfile: { screen: UserProfile },
-  Groups: { screen: Groups },
 });
 
 export default AppNavigator;
