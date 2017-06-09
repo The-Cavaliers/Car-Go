8cb5e48b11d6081e4e8873d938f1445d0290eab7
Phongtlam <Phongtlam@gmail.com>
2017-06-08 18:44:37 -0700

    working current version

--- client/reducers/LoginProfileReducer.js
+++ client/reducers/LoginProfileReducer.js
@@ -6,8 +6,7 @@ const INITIAL_STATE = {
   picture_url: '',
   token: '',
   social_provider: '',
-  created_at: '',
-  
+
 };
 
 function loginProfile(state = INITIAL_STATE, { type, data }) {
@@ -20,7 +19,6 @@ function loginProfile(state = INITIAL_STATE, { type, data }) {
         picture_url: data.picture_url,
         token: data.token,
         social_provider: data.social_provider,
-        created_at: data.created_at,
       };
     default:
       return state;
