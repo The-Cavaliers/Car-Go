527eef44b88f700510d3ee863749240ddeeacde1
Mahima Srikanta <mahima.srikanta@gmail.com>
2017-06-10 08:40:22 -0700

    Seed userData

--- userSeed.js
+++ userSeed.js
@@ -1,18 +1,19 @@
 const models = require('./db/models');
 
-const seedDataBase = function (argument) {
-  const username = ['Phong', 'Aly', 'Micheal', 'Mary', 'Shawn'];
+const seedUserDataBase = function (argument) {
+  const username = ['Phong', 'Aly', 'Micheal'];
   const from = ['Fremont', 'FosterCity', 'San Mateo'];
   const to = ['Oakland', 'San Francisco', 'Sunnyvale'];
   const group_id = [1, 2, 3];
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 5; i++) {
     new models.User({
-      username: username[Math.floor(Math.random() * 5)],
+      username: username[Math.floor(Math.random() * 3)],
       from: from[Math.floor(Math.random() * 3)],
       to: to[Math.floor(Math.random() * 3)],
       group_id: group_id[Math.floor(Math.random() * 3)],
     }).save();
   }
 };
-seedDataBase();
+seedUserDataBase();
+
