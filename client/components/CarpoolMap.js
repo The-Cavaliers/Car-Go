3cdc1f968b78f1ebac2af4a2950a859ce42f858a
Mahima Srikanta <mahima.srikanta@gmail.com>
2017-06-10 13:34:52 -0700

    PubNub updated version

--- client/components/CarpoolMap.js
+++ client/components/CarpoolMap.js
@@ -6,17 +6,12 @@ AsyncStorage,
 Mapview,
 StyleSheet,
 } from 'react-native';
-import PubNub from 'pubnub';
 import pick from 'lodash/pick';
 import MapView from 'react-native-maps';
 import DrawerButton from './DrawerButton';
 import axios from 'axios';
+import { addPubNubListener, addPubNubPublisher, unSubscribe, pubnubStop } from '../services/pubnubClient';
 
-const pubnub = new PubNub({
-  subscribe_key: 'sub-c-44e30d18-4c19-11e7-b7ac-02ee2ddab7fe',
-  publish_key: 'pub-c-584834c0-fe57-43ab-927b-f993708f8776',
-
-});
 class clientPubNub extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: 'Chatter Box',
@@ -40,26 +35,8 @@ class clientPubNub extends Component {
   }
 
   componentDidMount() {
-    console.log('Hello', AsyncStorage);
-    // Map
-    navigator.geolocation.getCurrentPosition((position) => {
-      const initialPosition = JSON.stringify(position);
-      this.setState({
-        region: {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-          loc: 0,
-        },
-        isMapVisible: true,
-      });
-    },
-    error => alert(JSON.stringify(error)),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-    );
-    // PubNub
-    console.log(AsyncStorage);
+    
+    console.log('Mahimammmmmm');
     AsyncStorage.getItem('MapGroup', (err, group_data) => {
       this.state.channelName = JSON.parse(group_data).group;
       this.state.channelUserRole = JSON.parse(group_data).role;
@@ -67,59 +44,23 @@ class clientPubNub extends Component {
         console.log('from carpool driver');
         this.watchUserPostion();
       } else {
-        this.addPubNubListener();
+        // this.addPubNubListener();
+        addPubNubListener(this.state.channelName);
+
       }
     });
   }
-  addPubNubListener() {
-    pubnub.addListener({
-      status(statusEvent) {
-        if (statusEvent.category === 'PNConnectedCategory') {
-          console.log('need to check');
-        } else if (statusEvent.category === 'PNUnknownCategory') {
-          pubnub.setState({
-            state: { new: 'error' },
-          }, (status) => {
-            console.log(statusEvent.errorData.message);
-          });
-        }
-      },
-      message(message) {
-        console.log('message', message);
-        // alert(message);
-      },
-    });
-    pubnub.subscribe({
 
-      channels: [this.state.channelName],
-    });
-  }
-
-  addPubNubPublisher(positionLatLngs) {
-    console.log('from pub');
-    pubnub.publish({
-      message: {
-        player: this.state.channelUserRole,
-        position: positionLatLngs,
-      },
-      channel: this.state.channelName,
-    },
-    (status, response) => {
-      if (status.error) {
-        console.log(status.errorData);
-      } else {
-        console.log('message Published w/ timetoken', response.timetoken);
-      }
-    });
-  }
   watchUserPostion() {
+   pubnubStop();
     this.watchID = navigator.geolocation.watchPosition((position) => {
       const { routeCoordinates } = this.state;
       const newLatLngs = { latitude: position.coords.latitude, longitude: position.coords.longitude };
       const positionLatLngs = pick(position.coords, ['latitude', 'longitude']);
       //this.setState({ routeCoordinates: routeCoordinates.concat(positionLatLngs) });
       // alert(positionLatLngs);
-      this.addPubNubPublisher(positionLatLngs);
+      //this.addPubNubPublisher(positionLatLngs);
+      addPubNubPublisher( positionLatLngs, this.state.channelName, this.state.channelUserRole )
     });
   }
 
