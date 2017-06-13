a7ed85a2d68269965d3da489405bcc42e530f8ed
Phongtlam <Phongtlam@gmail.com>
2017-06-13 16:25:45 -0700

    4:25

--- client/components/GroupList.js
+++ client/components/GroupList.js
@@ -29,7 +29,7 @@ class CreateList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      groups: [1,2,3],
+      groups: [],
       // group_id: '',
       showLoading: false
     }
