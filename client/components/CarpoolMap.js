c3a86481b2d1613c581e391e789b15cd5cc2962e
Mahima Srikanta <mahima.srikanta@gmail.com>
2017-06-14 07:01:04 -0700

    Car Movement

--- client/components/CarpoolMap.js
+++ client/components/CarpoolMap.js
@@ -1,9 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import {
-View,
-AsyncStorage,
-StyleSheet,
+  View,
+  AsyncStorage,
+  StyleSheet,
 } from 'react-native';
 import pick from 'lodash/pick';
 import MapView from 'react-native-maps';
@@ -13,7 +13,7 @@ import Polyline from '@mapbox/polyline';
 import axios from 'axios';
 import CONFIG from '../../config/development.json';
 import PubNub from 'pubnub';
-import {  Button, Text } from 'native-base';
+import { Button, Text } from 'native-base';
 
 class clientPubNub extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -30,14 +30,14 @@ class clientPubNub extends Component {
       isRouteTracking: false,
       routeCoordinates: [],
       coords: [],
-      riderCoords : {
-        latitude: 0,
-        longitude: 0,
+      riderCoords: {
+        latitude: 37.33414,
+        longitude: -122.047359,
       },
-      Destination : '',
+      Destination: '',
       Source: '',
       finalDestination: {},
-      groupPublisherEmail:'',
+      groupPublisherEmail: '',
     };
   }
   componentDidMount() {
@@ -49,22 +49,23 @@ class clientPubNub extends Component {
         subscribe_key: CONFIG.pubnub.subscribeKey,
         publish_key: CONFIG.pubnub.publishKey,
         //uuid:JSON.parse(group_data).userEmail,
-        uuid:"abc124 ",
+        uuid: "abc1245",
       });
-      
+
       //Get the Destination Address from Group List for Unsubscribe
       //this.state.Destination = JSON.parse(group_data).goingTo;
-      this.state.Destination = 'Hayward';
-      axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.Destination}&key=${CONFIG.GoogleGeocoder.key}`)
-      .then((data) => {
-        this.state.finalDestination = data.data.results[0].geometry.location;
-      })
-      .catch((error) => {
-        console.log('error from google', error);
-      });
+      // this.state.Destination = 'Hayward';
+      // axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.Destination}&key=${CONFIG.GoogleGeocoder.key}`)
+      // .then((data) => {
+      //   this.state.finalDestination = data.data.results[0].geometry.location;
+      // })
+      // .catch((error) => {
+      //   console.log('error from google', error);
+      // });
       if (this.state.channelUserRole === 'Driver') {
+        alert("calling driver");
         this.watchUserPostion();
-      } else if(this.state.channelUserRole === 'Rider') {
+      } else if (this.state.channelUserRole === 'Rider') {
         this.addPubNubListener(this.state.channelName, 'Rider');
       }
     });
@@ -78,102 +79,105 @@ class clientPubNub extends Component {
   componentWillUnmount() {
     this.state.isRouteTracking = false;
   }
- 
- //For Publishing 
+
+  //For Publishing 
   watchUserPostion() {
-    alert("From publisher");
     let counter = 0;
     // call the subscribe
-    
+    this.addPubNubListener(this.state.channelName, 'Driver');
     this.getPolyLineDetails();
     this.watchID = navigator.geolocation.watchPosition((position) => {
       let { routeCoordinates } = this.state;
       let newLatLngs = { latitude: position.coords.latitude, longitude: position.coords.longitude };
       const positionLatLngs = pick(position.coords, ['latitude', 'longitude']);
-      console.log(positionLatLngs);     
-      
+      console.log(positionLatLngs);
+
       //For Carpool live tracking on mount and unmount
-      if (this.state.isRouteTracking) { 
-          this.setState({ routeCoordinates: routeCoordinates.concat(positionLatLngs) });    
+      if (this.state.isRouteTracking) {
+        this.setState({ routeCoordinates: routeCoordinates.concat(positionLatLngs) });
       }
       //For conditional publishing with switched groups
       AsyncStorage.getItem('MapGroup', (err, group_data) => {
         if (this.state.channelName === JSON.parse(group_data).group) {
           //check if the destination has reached and unsubscribe
-          if((this.state.finalDestination.lat === positionLatLngs.latitude) && (this.state.finalDestination.lng === positionLatLngs.longitude)) {
+          if ((this.state.finalDestination.lat === positionLatLngs.latitude) && (this.state.finalDestination.lng === positionLatLngs.longitude)) {
             this.UnsubscribeRiders();
           }
-          this.addPubNubPublisher( positionLatLngs, this.state.channelName, this.state.channelUserRole )
+          this.addPubNubPublisher(positionLatLngs, this.state.channelName, this.state.channelUserRole)
         }
       });
     },
 
-    //Watch for every 1 sec of location change
-    (error) => this.setState({ error: error.message }),
+      //Watch for every 1 sec of location change
+      (error) => this.setState({ error: error.message }),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
     );
   }
 
 
-//For Subscribing
-  addPubNubListener = (channelName) => {
-    alert("From listener");
+  //For Subscribing
+  addPubNubListener = (channelName, Role) => {
+    // alert("From listener");
     const that = this;
-    let counter =0;
-     this.pubnub.addListener({
+    let counter = 0;
+    this.pubnub.addListener({
       status(statusEvent) {
         if (statusEvent.category === 'PNConnectedCategory') {
           console.log('need to checkk=============================');
         } else if (statusEvent.category === 'PNUnknownCategory') {
           this.pubnub.setState({
             state: { new: 'error' },
-          },(status) => {
-              console.log(statusEvent.errorData.message);
-            });
-          }
+          }, (status) => {
+            console.log(statusEvent.errorData.message);
+          });
+        }
       },
-    message(message) {
+      message(message) {
+        //discard the messages for Driver
+        if (Role === 'Rider') {
+          //For Carpool live tracking on mount and unmount
+          if (that.state.isRouteTracking) {
+            let { routeCoordinates } = that.state;
 
-      //For Carpool live tracking on mount and unmount
-      if (that.state.isRouteTracking) {
-        let { routeCoordinates } = that.state;
+            //Get the driver position to estimate the arrival timings initially 
+            if (counter === 0) {
+              that.getTimings(message.message.position);
+              counter++;
+            }
+            if (message.message.player === 'Driver') {
+              //Plot the received driver's positions on the map
+              that.setState({ routeCoordinates: routeCoordinates.concat(message.message.position) });
+            }
+          }
+        }
+      }
+    });
+    this.pubnub.subscribe({
+      channels: [channelName],
+      withPresence: true
+    });
 
-        //Get the driver position to estimate the arrival timings initially 
-        if(counter === 0) {
-          that.getTimings(message.message.position);
-          counter++;       
-        }
-        if (message.message.player === 'Driver') {
-          //Plot the received driver's positions on the map
-          that.setState({ routeCoordinates: routeCoordinates.concat(message.message.position) });
-        }
-      }    
-    }
-  });
-  this.pubnub.subscribe({
-    channels: [channelName],
-    withPresence: true
-  });
-  
-  //Check if the Driver has joined yet or not
-  this.pubnub.hereNow(
-    {
-      channels: [channelName], 
-      includeUUIDs: true,
-      includeState: true
-    },
-    function (status, response) {
+    //Check if the Driver has joined yet or not
+    this.pubnub.hereNow(
+      {
+        channels: [channelName],
+        includeUUIDs: true,
+        includeState: true
+      },
+      function (status, response) {
         // handle status, response
         let isDriverPresent = false;
-        console.log("from presence",response.channels[channelName]['occupants'])
-        response.channels[channelName]['occupants'].forEach(function(occupant) {
-          if ( (occupant.uuid === "abc124 ") && (counter > 1) ) {
+        console.log("from presence", response.channels[channelName]['occupants'])
+        response.channels[channelName]['occupants'].forEach(function (occupant) {
+          if ((occupant.uuid === "abc1245") && (counter > 1)) {
             isDriverPresent = true;
           }
         });
-      isDriverPresent ? null : alert("Driver not yet joined");
-    }
-  );
+        if (!isDriverPresent) {
+          alert("Driver did not start yet")
+        }
+      }
+    );
 
   };
 
@@ -188,13 +192,13 @@ class clientPubNub extends Component {
       channel: channelName,
       withPresence: true
     },
-    (status, response) => {
-      if (status.error) {
-        console.log(status.errorData);
-      } else {
-        console.log('message Published w/ timetoken', response.timetoken, channelName);
-      }
-    });
+      (status, response) => {
+        if (status.error) {
+          console.log(status.errorData);
+        } else {
+          console.log('message Published w/ timetoken', response.timetoken, channelName);
+        }
+      });
   };
 
   //Get the arrival timings of driver at Riders position 
@@ -203,23 +207,23 @@ class clientPubNub extends Component {
     const DriverPosition = currentDriverPosition.latitude + "," + currentDriverPosition.longitude;
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        var currentRiderPosition = ''+ position.coords.latitude + "," + position.coords.longitude;
+        var currentRiderPosition = '' + position.coords.latitude + "," + position.coords.longitude;
         this.setState({
-          riderCoords : {
+          riderCoords: {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
           }
         });
         axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${currentRiderPosition}&destination=${DriverPosition}&key=${key}`)
-        .then((data) => {
-          alert(`Driver will reach in ${data.data.routes[0].legs[0].duration.text}` );
-        })
-        .catch((error) => {
-          console.log('error from google', error);
-        });
+          .then((data) => {
+            alert(`Driver will reach in ${data.data.routes[0].legs[0].duration.text}`);
+          })
+          .catch((error) => {
+            console.log('error from google', error);
+          });
       },
       (error) => alert(error.message),
-        {enableHighAccuracy: true, timeout: 20000, maximumAge: 100}
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 100 }
     );
   }
 
@@ -231,17 +235,17 @@ class clientPubNub extends Component {
     const wayPoints = ['Foster City, CA', 'Redwood City. CA'];
     const key = CONFIG.GoogleGeocoder.key;
     axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLocation}&destination=${endLocation}&waypoints=optimize:true|${wayPoints}&key=${key}`)
-    .then((data) => {
-      const points = Polyline.decode(data.data.routes[0].overview_polyline.points);
-      const coords = points.map((point) => ({
-        latitude: point[0],
-        longitude: point[1],
-      }));
-      this.setState({coords: coords})
-    })
-    .catch((error) => {
-      console.log('error from google', error);
-    });
+      .then((data) => {
+        const points = Polyline.decode(data.data.routes[0].overview_polyline.points);
+        const coords = points.map((point) => ({
+          latitude: point[0],
+          longitude: point[1],
+        }));
+        this.setState({ coords: coords })
+      })
+      .catch((error) => {
+        console.log('error from google', error);
+      });
   };
 
   // Need to Implement Get all Riders pick up points
@@ -263,40 +267,50 @@ class clientPubNub extends Component {
   }
 
   render() {
+    console.log("Mapppppp", typeof (this.state.routeCoordinates.slice(-1)))
+    console.log("mapppppp22222", typeof (this.state.riderCoords));
+    var coord = this.state.routeCoordinates.slice(-1);
     return (
       <MapView
         style={styles.map}
-        showsUserLocation
+        showsUserLocation={true}
+        zoomEnabled={true}
         // followUserLocation
         showsCompass
         showsPointsOfInterest
         initialRegion={{
-          latitude:37.332211,
+          latitude: 37.332211,
           longitude: -122.030778,
           latitudeDelta: 0.0522,
           longitudeDelta: 0.0421
         }}
-        // overlays={[{
-        //   coordinates: this.state.routeCoordinates,
-        //   strokeColor: 'purple',
-        //   strokeWidth: 5
-        // }]}
-        >    
-        <MapView.Polyline 
+      // overlays={[{
+      //   coordinates: this.state.routeCoordinates,
+      //   strokeColor: 'purple',
+      //   strokeWidth: 5
+      // }]}
+      >
+        <MapView.Polyline
           coordinates={this.state.routeCoordinates}
-          strokeWidth={3}
-          strokeColor="blue"/>
+          strokeWidth={5}
+          strokeColor="blue" />
+
+        <MapView.Marker draggable
+          coordinate={this.state.riderCoords}
+          onDragEnd={(e) => this.setState({ x: e.nativeEvent.coordinate })}
+        />
 
         <MapView.Marker
-          coordinate={this.state.riderCoords}
-          title={"My Location"}
-          pinColor = "red"
+          coordinate={this.state.routeCoordinates[this.state.routeCoordinates.length-1]}
+          title={"Driver"}
+          image={require('../assets/Caar1.png')}
         />
+
         <Button small rounded danger onPress={() => this.UnsubscribeRiders()}>
           <Text>UnSubscribe</Text>
         </Button>
-      </MapView> 
-         
+      </MapView>
+
     );
   }
 }
