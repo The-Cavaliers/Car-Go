e91e9c13ca845a983cfdb18b4246a83c2473fe05
Phongtlam <Phongtlam@gmail.com>
2017-06-14 16:42:21 -0700

    fixing messenger

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,27 +1,11 @@
-// import { LOGIN_PROFILE, SET_PROFILE } from './type';
-
 import { setProfile, setLoginProfile } from './profileActions';
 import { getChatId } from './chatActions';
-
-// export const setProfile = (data) => {
-//   return {
-//     type: SET_PROFILE,
-//     data,
-//   };
-// };
 
 export const setProfileAsync = (data) => {
   return function (dispatch) {
     dispatch(setProfile(data));
   };
 };
-
-// const setLoginProfile = (data) => {
-//   return {
-//     type: LOGIN_PROFILE,
-//     data,
-//   };
-// };
 
 export const setLoginProfileAsync = (data) => {
   return function (dispatch) {
